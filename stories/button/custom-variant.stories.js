0fe7c7b064b1362b5f50692b39895078bb12649f
twebber90 <twebber@tractorzoom.com>
2021-03-24 14:14:54 -0500

    chore: remove storybook examples

--- stories/button/custom-variant.stories.js
+++
@@ -1,8 +0,0 @@
-import Button from '../../packages/button/src';
-import React from 'react';
-import propsMarkdown from '../utilities/props/button.md';
-import { storiesOf } from '@storybook/react';
-
-storiesOf('Button', module).add('Custom Variant', () => <Button title='Confirm' variant='outlined' />, {
-    notes: { markdown: propsMarkdown },
-});
--- stories/button/loading.stories.js
+++
@@ -1,8 +0,0 @@
-import Button from '../../packages/button/src';
-import React from 'react';
-import propsMarkdown from '../utilities/props/button.md';
-import { storiesOf } from '@storybook/react';
-
-storiesOf('Button', module).add('Loading Button', () => <Button isLoading={true} title='Confirm' />, {
-    notes: { markdown: propsMarkdown },
-});
