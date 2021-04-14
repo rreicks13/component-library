d8b6df4b4c771b0f81a5b8c5024f181aed18a41d
Ross Reicks <rreicks@tractorzoom.com>
2021-04-14 18:22:29 -0500

    fix: add logic for server style sheets

--- pages/_document.js
+++ pages/_document.js
@@ -1,5 +1,6 @@
 import Document, { Html, Head, Main, NextScript } from 'next/document';
 import React from 'react';
+import { ServerStyleSheets } from '@material-ui/core/styles';
 
 export default class MyDocument extends Document {
     render() {
@@ -16,3 +17,20 @@ export default class MyDocument extends Document {
         );
     }
 }
+
+MyDocument.getInitialProps = async (ctx) => {
+    const sheets = new ServerStyleSheets();
+    const originalRenderPage = ctx.renderPage;
+
+    ctx.renderPage = () =>
+        originalRenderPage({
+            enhanceApp: (App) => (props) => sheets.collect(<App {...props} />),
+        });
+
+    const initialProps = await Document.getInitialProps(ctx);
+
+    return {
+        ...initialProps,
+        styles: [...React.Children.toArray(initialProps.styles), sheets.getStyleElement()],
+    };
+};
