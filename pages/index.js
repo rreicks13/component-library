7dfdba526fa173d801a3610479de0476ad6076c9
twebber90 <twebber@tractorzoom.com>
2021-04-19 14:55:11 -0500

    feat: add examples for equipment card

--- pages/index.js
+++ pages/index.js
@@ -18,6 +18,9 @@ export default () => {
             <Link href='/button'>
                 <Button variant='contained'>@tractorzoom/button</Button>
             </Link>
+            <Link href='/equipment-card'>
+                <Button variant='contained'>@tractorzoom/equipment-card</Button>
+            </Link>
             <Link href='/places-select'>
                 <Button variant='contained'>@tractorzoom/places-select</Button>
             </Link>
