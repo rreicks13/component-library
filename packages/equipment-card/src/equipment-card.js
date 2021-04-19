f87b7f0e63a027390f00d92b4d2a9a67e713568f
twebber90 <twebber@tractorzoom.com>
2021-04-19 14:54:51 -0500

    feat: add equipment card component

---
+++ packages/equipment-card/src/equipment-card.js
@@ -0,0 +1,137 @@
+import { formatNumberWithThousandSeparator, getLocation, getVariableDetail } from '@tractorzoom/equipment-attributes';
+import AddRoundedIcon from '@material-ui/icons/AddRounded';
+import Card from '@material-ui/core/Card';
+import CardActionArea from '@material-ui/core/CardActionArea';
+import CardContent from '@material-ui/core/CardContent';
+import CardMedia from '@material-ui/core/CardMedia';
+import CheckRoundedIcon from '@material-ui/icons/CheckRounded';
+import Divider from '@material-ui/core/Divider';
+import IconButton from '@material-ui/core/IconButton';
+import LocationOnIcon from '@material-ui/icons/LocationOn';
+import PropTypes from 'prop-types';
+import React from 'react';
+import Typography from '@material-ui/core/Typography';
+import clsx from 'clsx';
+import moment from 'moment';
+import useStyles from './styles';
+
+const EquipmentCard = (props) => {
+    const classes = useStyles();
+    const saleDate = props.saleDate ? props.saleDate : props.auctionDate;
+    const formattedDate = moment(saleDate).format('L');
+    const isSelected = props.selectedEquipmentSet.has(props.id);
+    const variableDetail = getVariableDetail(props);
+    const variableDetailStyle = variableDetail.length >= 18 ? { fontSize: 14 } : {};
+
+    const handleToggleSelected = (event) => {
+        props.handleEquipmentSelected();
+        event.stopPropagation();
+    };
+
+    return (
+        <Card
+            className={clsx(classes.root, {
+                [classes.selectedCard]: isSelected,
+            })}
+            data-cy='equipment-card'
+            data-guid={props.id}
+            style={props.style}
+            variant='outlined'
+        >
+            <CardActionArea
+                data-tour={props.shouldHaveDataTour ? 'equipment-card-action-area' : ''}
+                onClick={props.handleOpen}
+            >
+                <IconButton
+                    aria-label='select equipment'
+                    className={isSelected ? classes.checkedButton : classes.selectButton}
+                    color='primary'
+                    data-cy='equipment-card-toggle-selection-button'
+                    data-tour={props.shouldHaveDataTour ? 'equipment-card-select-equipment' : ''}
+                    onClick={handleToggleSelected}
+                    title={isSelected ? 'Remove from custom average' : 'Add to custom average'}
+                >
+                    {isSelected ? <CheckRoundedIcon fontSize='large' /> : <AddRoundedIcon fontSize='large' />}
+                </IconButton>
+                <CardMedia
+                    className={classes.media}
+                    component='img'
+                    data-cy='equipment-card-image'
+                    onError={(e) => {
+                        e.target.onerror = null;
+
+                        if (e.target.src === props.makeImageUrl || !props.makeImageUrl) {
+                            e.target.src = '/img/nopicture.png';
+                        } else {
+                            e.target.src = props.makeImageUrl;
+                        }
+                    }}
+                    src={props.imageUrl}
+                    title='Equipment Image'
+                />
+                <CardContent classes={{ root: classes.cardContent }}>
+                    <Typography
+                        className={classes.makeModelTitle}
+                        component='h2'
+                        data-cy='equipment-card-make-model'
+                        variant='h5'
+                    >
+                        {`${props.year ? `${props.year} ` : ''}${props.make} ${props.model}`}
+                    </Typography>
+                    <div className={classes.details}>
+                        <Typography
+                            className={classes.variableDetail}
+                            data-cy='equipment-card-variable-detail'
+                            style={variableDetailStyle}
+                        >
+                            {variableDetail}
+                        </Typography>
+                        <Typography className={classes.price} data-cy='equipment-card-price'>
+                            {`$${formatNumberWithThousandSeparator(`${props.price}`)}`}
+                        </Typography>
+                    </div>
+                    <Divider />
+                    <div className={classes.locationAndSaleDate}>
+                        <Typography className={classes.detailsText} data-cy='equipment-card-location'>
+                            <LocationOnIcon style={{ height: 16, width: 16 }} />
+                            {getLocation(props)}
+                        </Typography>
+                        <Typography className={classes.detailsText} data-cy='equipment-card-sale-date'>
+                            Sold {formattedDate}
+                        </Typography>
+                    </div>
+                </CardContent>
+            </CardActionArea>
+        </Card>
+    );
+};
+
+EquipmentCard.defaultProps = {
+    auctionDate: '',
+    distance: null,
+    imageUrl: '',
+    saleDate: '',
+    shouldHaveDataTour: false,
+    year: null,
+};
+
+EquipmentCard.propTypes = {
+    auctionDate: PropTypes.string,
+    distance: PropTypes.number,
+    handleOpen: PropTypes.func.isRequired,
+    handleEquipmentSelected: PropTypes.func.isRequired,
+    id: PropTypes.oneOfType([PropTypes.number, PropTypes.string]).isRequired,
+    imageUrl: PropTypes.string.isRequired,
+    make: PropTypes.string.isRequired,
+    makeImageUrl: PropTypes.string.isRequired,
+    model: PropTypes.string.isRequired,
+    price: PropTypes.number.isRequired,
+    saleDate: PropTypes.string,
+    selectedEquipmentSet: PropTypes.object.isRequired,
+    shouldHaveDataTour: PropTypes.bool,
+    state: PropTypes.string,
+    style: PropTypes.object,
+    year: PropTypes.number,
+};
+
+export default EquipmentCard;
---
+++ packages/equipment-card/src/hook.js
@@ -0,0 +1,93 @@
+import CancelRoundedIcon from '@material-ui/icons/CancelRounded';
+import IconButton from '@material-ui/core/IconButton';
+import InfoRoundedIcon from '@material-ui/icons/InfoRounded';
+import InputAdornment from '@material-ui/core/InputAdornment';
+import React from 'react';
+import clsx from 'clsx';
+import useStyles from './styles';
+
+const useEquipmentCard = (props) => {
+    const classes = useStyles();
+    const {
+        InputLabelProps,
+        InputProps,
+        step,
+        suffixLabel,
+        suffixOnChange,
+        suffixOptions,
+        suffixValue,
+        ...otherProps
+    } = props;
+    const labelClassName = otherProps.value && !otherProps.error ? classes.activeLabel : '';
+    const inputLabelPropsObj = {
+        ...InputLabelProps,
+        className: InputLabelProps.className ? clsx(labelClassName, InputLabelProps.className) : labelClassName,
+    };
+    const inputPropsObj = {};
+    const clear = () => {
+        otherProps.onChange({
+            target: {
+                value: '',
+            },
+        });
+    };
+    const errorOrClearAdornment = otherProps.error ? (
+        <InputAdornment position='end'>
+            <InfoRoundedIcon className={classes.errorIcon} />
+        </InputAdornment>
+    ) : otherProps.value && !otherProps.disabled ? (
+        <InputAdornment position='end'>
+            <IconButton onClick={clear}>
+                <CancelRoundedIcon className={classes.clearIcon} />
+            </IconButton>
+        </InputAdornment>
+    ) : null;
+    const endAdornment = suffixLabel ? (
+        <InputAdornment className={classes.suffixLabel} position='end'>
+            {suffixLabel}
+        </InputAdornment>
+    ) : (
+        errorOrClearAdornment
+    );
+
+    if (otherProps?.type === 'number') {
+        inputPropsObj.step = step;
+    }
+
+    if (otherProps?.type === 'date') {
+        inputLabelPropsObj.shrink = true;
+    }
+
+    const updatedInputProps = {
+        ...InputProps,
+        className: clsx(classes.input, {
+            [classes.optionsInput]: suffixOptions.length,
+            [InputProps.className]: Boolean(InputProps.className),
+        }),
+        classes: {
+            adornedEnd: classes.adornedEnd,
+            error: classes.error,
+            focused: classes.focused,
+        },
+        disableUnderline: true,
+        endAdornment,
+        inputProps: inputPropsObj,
+    };
+
+    return {
+        ...otherProps,
+        optionsTextFieldClassName: suffixOptions.length ? classes.optionsTextField : '',
+        InputLabelProps: inputLabelPropsObj,
+        InputProps: updatedInputProps,
+        optionsSelectProps: {
+            className: classes.optionsSelect,
+            SelectProps: { classes: { select: classes.optionsSelectLabel } },
+            InputProps: { className: classes.optionsSelectInput },
+            options: suffixOptions,
+            onChange: suffixOnChange,
+            value: suffixValue,
+        },
+    };
+};
+
+export default useEquipmentCard;
